c0bb2f2a3e7db8f13e5ac34ab8e2992d6993f6b0
S-ashraf-hussain <59016159+S-ashraf-hussain@users.noreply.github.com>
2019-12-18 17:02:49 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+var myApp = angular.module('myApp',[]);
+myApp.controller('myController',['$scope','$filter',function($scope , $filter){
+    $scope.decimal="";
+    $scope.binary="";
+    $scope.octal="";
+    $scope.hexa="";
+
+    $scope.$watch('decimal', function(newValue , oldValue){
+     // console.log("abc");
+     if(newValue != ""){
+
+     $scope.binary = parseInt(newValue , 10).toString(2);
+     $scope.octal = parseInt(newValue , 10).toString(8);
+     $scope.hexa = parseInt(newValue , 10).toString(16).toUpperCase();
+       }else
+       {
+     $scope.decimal="";
+     $scope.binary="";
+     $scope.octal="";
+     $scope.hexa="";
+       }
+
+    });
+
+    $scope.$watch('binary', function(newValue , oldValue){
+     // console.log("abc");
+      if(newValue != ""){
+     $scope.decimal = parseInt(newValue , 2).toString(10);
+      }else
+       {
+       	$scope.decimal="";
+       }
+    });
+
+    $scope.$watch('octal', function(newValue , oldValue){
+     // console.log("abc");
+     if(newValue != ""){
+     $scope.decimal = parseInt(newValue , 8).toString(10);
+      }else
+       {
+       	$scope.decimal="";
+       }
+    });
+
+    $scope.$watch('hexa', function(newValue , oldValue){
+     // console.log("abc");
+     if(newValue != ""){
+     $scope.decimal = parseInt(newValue , 16).toString(10);
+     }else
+       {
+       	$scope.decimal="";
+       }
+    });
+}]);
